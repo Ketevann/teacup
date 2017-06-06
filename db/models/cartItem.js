5fa2b28f24bc32a106440e21a24ea039b272dd56
Ketevann <ketevan.tsin@gmail.com>
2017-06-06 15:42:55 -0400

    deleted package.json

---
+++ db/models/cartItem.js
@@ -0,0 +1,14 @@
+'use strict'
+const Sequelize = require('sequelize')
+const db = require('../index.js')
+
+
+module.exports = db.define('cartItem', {
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+})
+
+//??
+//Product.hasMany(Order,{through: cartItem});
