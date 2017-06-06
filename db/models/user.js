d7feceebef1291ed3a37efccc42b32cf20070c60
Ketevann <ketevan.tsin@gmail.com>
2017-06-06 16:40:24 -0400

    updated user

--- db/models/user.js
+++ db/models/user.js
@@ -1,28 +1,28 @@
-'use strict';
-const Sequelize = require('sequelize');
-var db = require('../index.js');
+'use strict'
+const Sequelize = require('sequelize')
+const db = require('../index.js')
 
 
 const User = db.define('user', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING,
-        validate: {
-            isEmail: true,
-            unique: true
-        }
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    isAdmin: {
-        type: Sequelize.BOOLEAN,
-        defaulValue: false
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true,
+      unique: true
     }
-});
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaulValue: false
+  }
+})
 
-module.exports = User;
+module.exports = User
