f034b7d161da0a40b9b665de78ba5e8a4efa4506
Claire Pfister <clairepfis@gmail.com>
2017-06-06 16:32:52 -0400

    made consts

--- db/models/review.js
+++ db/models/review.js
@@ -1,6 +1,6 @@
 'use strict'
-var Sequelize = require('sequelize')
-var db = require('../index.js')
+const Sequelize = require('sequelize')
+const db = require('../index.js')
 
 module.exports = db.define('review', {
     content: {
