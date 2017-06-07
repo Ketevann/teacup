347ddc7993ff7f1e3d381f23fa80e352b06e8442
Claire Pfister <clairepfis@gmail.com>
2017-06-07 18:01:19 -0400

    some routes

---
+++ server/cartItem.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+const Orders = require('../db/models/order'); 
+
+
+//add post when do oauth 
+router.get('/cartItems/:orderId',function(req,res,next){
+	CartItem.findAll({
+		where: {order_Id : req.params.id},
+	    include:[Products]
+		})
+	    .then((items)=>res.send(items))
+	    .catch(console.err)
+
+
+})
---
+++ server/order.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const Orders = require('../db/models/order');
+
+router.get('/orders',function(req,res,next){
+	Orders.findAll({}).then((orders)=>{res.send(orders)})
+		.catch(console.err)
+})
+router.get('/order/:orderId',function(req,res,next){
+	Orders.findAll({
+		where:{
+			id: req.params.orderId
+	    }
+    })
+    .then((orders)=>{res.send(orders)})
+    .catch(console.err)
+})
+router.get('/order/:userId',function(req,res,next){
+	Orders.findAll({
+		where:{
+			user_Id: req.params.userId
+	    }
+    })
+    .then((orders)=>{res.send(orders)})
+    .catch(console.err)
+})
