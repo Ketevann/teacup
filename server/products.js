bea28aa1abca2dd8597c026a6f146727d427a7dd
Ketevann <ketevan.tsin@gmail.com>
2017-06-07 16:47:23 -0400

    made syntax changes

--- server/products.js
+++ server/products.js
@@ -6,7 +6,7 @@ var Products = require('../db/models').product
 api.get('/products', (req, res, next){
   return Products.findAll({})
     .then((products) => {
-      if (products === null)
+      if (!products)
         res.status(404).send("Page not found")
       else res.send(products)
     })
@@ -16,7 +16,7 @@ api.get('/products', (req, res, next){
 api.get('/products/:id', (req, res, next) => {
   Products.findById(req.params.id)
     .then(product => {
-      if (product === null) res.status(404).send("Page not found")
+      if (!product) res.status(404).send("Page not found")
       else res.send(product)
     })
     .catch(console.error())
@@ -29,23 +29,8 @@ api.get('/product', (req, res, next) => {
     }
   })
     .then(product => {
-      if (product === null) res.status(404).send("Page not found")
+      if (!product) res.status(404).send("Page not found")
       else res.send(product)
-    })
-    .catch(console.error())
-}),
-
-api.delete('/product', (req, res, next) => {
-  return Products.findOne({
-    where: {
-      name: req.body.name
-    }
-  })
-    .then(product => {
-      return product.destroy({})
-    })
-    .then(() => {
-      res.send(204)
     })
     .catch(console.error())
 }),
