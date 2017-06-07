bd12963b1d395bb7a3d1627c1605c1e843a984d7
Ketevann <ketevan.tsin@gmail.com>
2017-06-07 15:51:22 -0400

    created routes, but the code has  both get by id and get by name product routes.

--- db/models/payment.js
+++ db/models/payment.js
@@ -1,6 +1,6 @@
 'use strict'
-const Sequelize = require('sequelize');
-const db = require('../index.js');
+const Sequelize = require('sequelize')
+const db = require('../index.js')
 
 
 module.exports = db.define('payment', {
@@ -44,5 +44,5 @@ module.exports = db.define('payment', {
   },
   zipCode: {
     type: Sequelize.INTEGER
-  }
-});
+  },
+})
---
+++ server/products.js
@@ -0,0 +1,62 @@
+const api = require('express').Router()
+var Products = require('../db/models').product
+
+
+
+api.get('/products', (req, res, next){
+  return Products.findAll({})
+    .then((products) => {
+      if (products === null)
+        res.status(404).send("Page not found")
+      else res.send(products)
+    })
+    .catch(console.error())
+}),
+
+api.get('/products/:id', (req, res, next) => {
+  Products.findById(req.params.id)
+    .then(product => {
+      if (product === null) res.status(404).send("Page not found")
+      else res.send(product)
+    })
+    .catch(console.error())
+}),
+
+api.get('/product', (req, res, next) => {
+  return Products.findOne({
+    where: {
+      name: req.body.name
+    }
+  })
+    .then(product => {
+      if (product === null) res.status(404).send("Page not found")
+      else res.send(product)
+    })
+    .catch(console.error())
+}),
+
+api.delete('/product', (req, res, next) => {
+  return Products.findOne({
+    where: {
+      name: req.body.name
+    }
+  })
+    .then(product => {
+      return product.destroy({})
+    })
+    .then(() => {
+      res.send(204)
+    })
+    .catch(console.error())
+}),
+
+api.delete('/product/:id', (req, res, next) => {
+  Product.findById(req.params.id)
+    .then(product => {
+      return product.destroy({})
+    })
+    .then(() => {
+      res.send(204)
+    })
+    .catch(console.error())
+})
