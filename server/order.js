f741f94f63e92f8329c4a4e0d9890ffd1d0e5a4e
Ketevann <ketevan.tsin@gmail.com>
2017-06-09 11:44:41 -0400

    updated order

--- server/order.js
+++ server/order.js
@@ -1,49 +1,51 @@
 const router = require('express').Router()
 const Orders = require('../db/models/order')
 
-router.get('/orders',function(req,res,next){
-	Orders.findAll({})
-	    .then((orders)=>{
-	    	if(!items.length){
-	    		var error = new Error();
-	    		error.status = 404;
-	    		throw error;
-	    	}
-	    	res.send(orders)
-	    })
-		.catch(next)
-})
-router.get('/order/:orderId',function(req,res,next){
-	Orders.findAll({
-		where:{
-			id: req.params.orderId
-	    }
+module.exports = require('express').Router()
+ .get('/orders',
+ (req, res, next) =>
+  Orders.findAll({})
+    .then((orders) => {
+      if (!orders.length) {
+        let error = new Error()
+        error.status = 404
+        throw error
+      }
+      res.send(orders)
     })
-    .then((orders)=>{
-    	if(!orders.length){
-	    		var error = new Error();
-	    		error.status = 404;
-	    		throw error;
-	    }
-    	res.send(orders)
+   .catch(next))
+ .get('/order/:orderId',
+    (req, res, next) => {
+      Orders.findAll({
+        where: {
+          id: req.params.orderId
+        }
+      })
+ .then((orders) => {
+   if (!orders.length) {
+     var error = new Error()
+     error.status = 404
+     throw error
+   }
+   res.send(orders)
+ })
+ .catch(next)
     })
-    .catch(next)
-})
-router.get('/order/:userId',function(req,res,next){
-	Orders.findAll({
-		where:{
-			user_Id: req.params.userId
-	    }
-    })
-    .then((orders)=>{
-    	if(!items.length){
-	    		var error = new Error();
-	    		error.status = 404;
-	    		throw error;
-	    }
-    	res.send(orders)
-    })
-    .catch(next)
-})
+ .get('/order/:userId',
+  (req, res, next) =>
+  Orders.findAll({
+    where: {
+      user_Id: req.params.userId
+    }
+  })
+ .then((orders) => {
+   if (!orders.length) {
+     var error = new Error()
+     error.status = 404
+     throw error
+   }
+   res.send(orders)
+ })
+ .catch(next))
 
 
