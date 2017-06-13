4f579e8774ea94467a9d442d67bda1d16ba30b26
Ketevann <ketevan.tsin@gmail.com>
2017-06-13 16:58:51 -0400

    modified users

--- app/components/Users.jsx
+++ app/components/Users.jsx
@@ -1,114 +1,58 @@
 import React, { Component } from 'react'
-
 import {} from 'APP/app/reducers/auth'
-
 import {connect} from 'react-redux'
-
 import {fetchUsers, deleteUser} from '../reducers/users'
-
 import {Link} from 'react-router'
-
-
 // only the  user names are "links" because formatting kept getting messed up when i wrapped
-
 //each row (with email, role and name ) in Link tags.
-
 const Users = ({users}, props) => {
   // console.log('!!!!!!!!!!!')
   // console.log('usr', users)
   // console.log('bla')
   // console.log('???')
   if (users.role === 'user') {
-
     return <div>!</div>
-
   }
-
   return (
-
       <div>
-
         <table className="table">
-
           <thead className="thead-inverse">
-
             <tr>
-
               <th>#</th>
-
               <th>First Name</th>
-
               <th>Email</th>
-
               <th>Role</th>
-
               <th></th>
-
             </tr>
-
           </thead>
-
           {users.length ? users.map((user, index) => {
-
             return (
-
               <tbody>
-
                 <tr>
-
                   <th scope="row">{index+1}</th>
-
                   <Link to={`/users/${user.id}`}>
-
                     <td ></td>
-
                     <td >{user.name}</td>
-
                   </Link>
-
                   <td>{user.email}</td>
-
                   <td>{user.role}</td>
-
                   <button  onClick={()=> ClickHandler
-
                       (user.id)}className="glyphicon glyphicon-remove"></button>
-
                   <button className="glyphicon glyphicon-edit"></button>
-
                 </tr>
-
               </tbody>)
-
           })
-
           : null}
-
       </table>
-
-
      </div>)
-
 }
-
-
 const ClickHandler = (userId)  => {
-
     //console.log(userId, 'UserId', deleteUser)
-
     console.log("DSDS")
-
     deleteUser(userId)
-
     console.log("invoked")
-
   }
-
-
 export default connect(
-
    ({ users }) => ({ users: users }),
-
   {fetchUsers},
-
 )(Users)
